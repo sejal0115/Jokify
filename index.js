5a1e44b0bc958ddfff3a155b9d13f6646fbf65e2
Sejal Mahadik <sejalmahadik1519@gmail.com>
2025-06-17 16:13:06 +0530

    New twopart added

--- index.js
+++ index.js
@@ -19,16 +19,24 @@ app.get("/", (req, res) => {
 
 
 app.post("/joke", async (req, res) => {
-      const category = req.body.category || "Any";
+    const category = req.body.category || "Any";
     try {
-        const response = await axios.get(`https://v2.jokeapi.dev/joke/${category}?type=single`);
-        const joke = response.data.joke;
+        const response = await axios.get(`https://v2.jokeapi.dev/joke/${category}?`);
+        let joke;
+
+        if (response.data.type === "single") {
+            joke = response.data.joke;
+        } else if (response.data.type === "twopart") {
+            joke = `${response.data.setup} 🤔 ${response.data.delivery}`;
+        }
+
         res.render("index", { joke });
     } catch (error) {
         console.error(error.message);
         res.render("index", { joke: "Sorry, couldn't fetch a joke." });
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
